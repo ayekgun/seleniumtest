f79a5845b191d9bf06160702de0f74d5da4863db
ayekgun <ayekgun1@gmail.com>
2022-02-15 00:48:27 +0700

    second commit

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
 let {Builder, By, Key, until} = require('selenium-webdriver');
 // let chrome = require('selenium-webdriver/chrome');
 
+
 require('chromedriver');
-async function helloSelenium(td) {
+async function helloSelenium(user, pass) {
     let driver = await new Builder().forBrowser('chrome').build();
 
 
     await driver.get('https://evermos.com/login');
     // login = driver.findElement(By.linkText('Masuk'));
     // await login.click();
-    inputs1 = driver.findElement(By.xpath("//input[@value='']")).sendKeys(td.username);
-    inputs2 = driver.findElement(By.xpath("(//input[@value=''])[2]")).sendKeys(td.password);
+    inputs1 = driver.findElement(By.xpath("//input[@value='']")).sendKeys(user);
+    inputs2 = driver.findElement(By.xpath("(//input[@value=''])[2]")).sendKeys(pass);
     btnLogin = driver.findElement(By.xpath("//button[@type='button']"));
-
-    setTimeout(() => {
-        btnLogin.click();
-    }, 20000);
-    // await driver.quit();
+    btnLogin.click();
 };
 
 
@@ -35,6 +32,9 @@ let testData = [
     },
 ]
 
-testData.map(async function(td){
-    await helloSelenium(td)
-})
+async function runs(){
+    await helloSelenium('salah', 'password');
+    await helloSelenium('6281223334444', 1234);
+    await helloSelenium('6281223334444', 'password');
+}
+runs();
